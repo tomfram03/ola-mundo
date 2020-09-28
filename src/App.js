90e1e6feaf43476683629f32924d2e30392aaf30
tomfram03 <tomfram03@gmail.com>
2020-09-28 09:31:01 -0300

    git

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>Olá, Mundo!!!</h1>
+        <h1>Olá, Mundo!!</h1>
         <a
           className="App-link"
           href="https://reactjs.org"
